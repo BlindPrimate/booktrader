e75189c893cbf972f5f7ba4bb93fcb0c8f91cb15
BlindPrimate <ecleek@gmail.com>
2015-10-14 15:18:09 -0400

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/bookbroker-dev'
+  },
+
+  seedDB: true
+};
